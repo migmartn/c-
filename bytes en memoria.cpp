aa6aeb72114d2a9bccede190784c28f2ad87cb65
migmart <72838534+migmartn@users.noreply.github.com>
2023-03-29 09:45:19 -0600

    Update bytes en memoria.cpp

--- bytes en memoria.cpp
+++ bytes en memoria.cpp
@@ -1,7 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<conio.h>
 int main(){
-	char number = 'a';
-	printf("%lu\n",sizeof(number));
+	int number, opc, x=1;
+	char caracter ;
+	while(x==1){
+	printf("\nopcion 1: ");
+	printf("\nopcion 2: ");
+	printf("\nelige una opcion: ");
+	scanf("%i", &opc);
+	if(opc==1)
+{
+	printf("\nIngresa un numero: ");
+	scanf("%i", &number);
+	printf(" espacio en memoria,""%lu\n",sizeof(number));
+}
+if(opc==2){
+    printf("Ingresa un caracter: ");
+	scanf("%s", &caracter);
+	printf("espacio en memoria, ""%lu\n",sizeof(caracter));
+}
+}
 	return 0;
+	getch();
 }
